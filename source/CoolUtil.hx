a3926e3e08ef10451a1f2971a080c9cf8d504939
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-09-21 23:01:23 +0300

    if ben ok's shit not works we will use this

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -21,7 +21,7 @@ class CoolUtil
 	public static function coolTextFile(path:String):Array<String>
 	{
 		var daList:Array<String> = []; 
-                if(FileSystem.exists(path)) daList = File.getContent(path).trim().split('\n');
+                var daList = File.getContent(SUtil.getPath() + path).trim().split('\n');
                  
 		for (i in 0...daList.length)
 		{
