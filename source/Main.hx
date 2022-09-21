f31660b2b30a8984fc2c072f151826e885940b09
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-09-21 21:42:43 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -71,6 +71,8 @@ class Main extends Sprite
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
+                SUtil.uncaughtErrorHandler();
+
 		addChild(new FPS(10, 3, 0xFFFFFF));
 	}
 }
