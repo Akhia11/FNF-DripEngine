e751b0fab6db5cb10e53fe519314714d0f4aa600
kamodrip <100513117+Akhia11@users.noreply.github.com>
2022-09-22 18:44:05 +0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -181,7 +181,7 @@ class MainMenuState extends MusicBeatState
 										FlxG.switchState(new StoryMenuState());
 										trace("Story Menu Selected");
                                                                         case 'mods':
-										FlxG.switchState(new ModsMenuState;
+										FlxG.switchState(new ModsMenuState());
 										trace("Mods Menu Selected");
 									case 'freeplay':
 										FlxG.switchState(new FreeplayState());
