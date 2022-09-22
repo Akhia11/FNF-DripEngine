365df50d223c0bf3d1d278e0e6c5f6fde42d091d
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-09-22 18:48:17 +0300

    chain saw better :trollface:

--- source/ModsMenuState.hx
+++ source/ModsMenuState.hx
@@ -1,177 +1,162 @@
-package states;
+package;
 
-#if sys
-import ui.ModIcon;
-import modding.ModList;
-import modding.PolymodHandler;
-import substates.UISkinSelect;
-import substates.ControlMenuSubstate;
-import modding.CharacterCreationState;
-import utilities.MusicUtilities;
-import ui.Option;
-import ui.Checkbox;
-import flixel.group.FlxGroup;
-import debuggers.ChartingState;
-import debuggers.StageMakingState;
-import flixel.system.FlxSound;
-import debuggers.AnimationDebug;
-import utilities.Controls.Control;
-import flash.text.TextField;
 import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.addons.display.FlxGridOverlay;
+import flixel.graphics.FlxGraphic;
+import flixel.group.FlxSpriteGroup;
 import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.input.keyboard.FlxKey;
-import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
-import lime.utils.Assets;
-import ui.Alphabet;
-import game.Song;
-import debuggers.StageMakingState;
-import game.Highscore;
+import openfl.display.BitmapData;
+import haxe.io.Bytes;
 
 class ModsMenuState extends MusicBeatState
 {
-	var curSelected:Int = 0;
-
-	public var page:FlxTypedGroup<ModOption> = new FlxTypedGroup<ModOption>();
-
-	public static var instance:ModsMenu;
-
-	var descriptionText:FlxText;
-	var descBg:FlxSprite;
+	private var daMods:FlxTypedGroup<Alphabet>;
+	private var iconArray:Array<ModIcon> = [];
+	private var curSelected:Int = 0;
+	private var changedSomething:Bool = false;
 
 	override function create()
 	{
-		MusicBeatState.windowNameSuffix = " Mods Menu";
+		changedSomething = false;
 
-		instance = this;
+		Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
 
-		var menuBG:FlxSprite;
+		#if FUTURE_DISCORD_RCP
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("In the Menus", null);
+		#end
 
-		menuBG = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		if (FlxG.sound.music != null && !FlxG.sound.music.playing)
+			FlxG.sound.playMusic(Paths.music('freakyMenu'));
 
-		menuBG.color = 0xFFea71fd;
-		menuBG.setGraphicSize(Std.int(menuBG.width * 1.1));
-		menuBG.updateHitbox();
-		menuBG.screenCenter();
-		menuBG.antialiasing = true;
-		add(menuBG);
+		persistentUpdate = persistentDraw = true;
 
-                #if android
-		addVirtualPad(UP_DOWN, A);
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.color = 0xFFea71fd;
+		add(bg);
+
+		daMods = new FlxTypedGroup<Alphabet>();
+		add(daMods);
+
+		for (i in 0...ModCore.trackedMods.length)
+		{
+			var text:Alphabet = new Alphabet(0, (70 * i) + 30, ModCore.trackedMods[i].title, false, false);
+			text.isMenuItem = true;
+			text.targetY = i;
+			daMods.add(text);
+
+			var icon:ModIcon = new ModIcon(ModCore.trackedMods[i].icon);
+			icon.sprTracker = text;
+			iconArray.push(icon);
+			add(icon);
+		}
+ 
+		changeSelection();
+
+		#if android
+		addVirtualPad(UP_DOWN, A_B);
 		#end
 
 		super.create();
-
-		add(page);
-
-		if(FlxG.sound.music == null)
-			FlxG.sound.playMusic(Paths.music('freakyMenu'), true);
-
-		PolymodHandler.loadModMetadata();
-
-		loadMods();
-
-		descBg = new FlxSprite(0, FlxG.height - 90).makeGraphic(FlxG.width, 90, 0xFF000000);
-		descBg.alpha = 0.6;
-		add(descBg);
-
-		descriptionText = new FlxText(descBg.x, descBg.y + 4, FlxG.width, "Template Description", 18);
-		descriptionText.setFormat(Paths.font("vcr.ttf"), 18, FlxColor.WHITE, CENTER);
-		descriptionText.borderColor = FlxColor.BLACK;
-		descriptionText.borderSize = 1;
-		descriptionText.borderStyle = OUTLINE;
-		descriptionText.scrollFactor.set();
-		descriptionText.screenCenter(X);
-		add(descriptionText);
-
-		var leText:String = "Press ENTER to enable / disable the currently selected mod.";
-
-		var text:FlxText = new FlxText(0, FlxG.height - 22, FlxG.width, leText, 18);
-		text.setFormat(Paths.font("vcr.ttf"), 18, FlxColor.WHITE, RIGHT);
-		text.scrollFactor.set();
-		text.borderColor = FlxColor.BLACK;
-		text.borderSize = 1;
-		text.borderStyle = OUTLINE;
-		add(text);
 	}
 
-	function loadMods()
+	override function update(elapsed:Float)
 	{
-		page.forEachExists(function(option:ModOption)
-		{
-			page.remove(option);
-			option.kill();
-			option.destroy();
-		});
+		if (FlxG.sound.music.volume < 0.7)
+			FlxG.sound.music.volume += 0.5 * elapsed;
 
-		var optionLoopNum:Int = 0;
+		if (controls.UP_P)
+			changeSelection(-1);
+		else if (controls.DOWN_P)
+			changeSelection(1);
+		else if (FlxG.mouse.wheel != 0)
+			changeSelection(-FlxG.mouse.wheel);
 
-		for(modId in PolymodHandler.metadataArrays)
+		if (controls.BACK)
 		{
-			var modOption = new ModOption(ModList.modMetadatas.get(modId).title, modId, optionLoopNum);
-			page.add(modOption);
-			optionLoopNum++;
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+			if (changedSomething)
+				ModCore.reload();
+			MusicBeatState.switchState(new MainMenuState());
 		}
+		else if (controls.ACCEPT)
+		{
+			if (!FlxG.save.data.disabledMods.contains(ModCore.trackedMods[curSelected].id))
+			{
+				FlxG.save.data.disabledMods.push(ModCore.trackedMods[curSelected].id);
+				FlxG.save.flush();
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				changeSelection();
+				changedSomething = !changedSomething;
+			}
+			else
+			{
+				FlxG.save.data.disabledMods.remove(ModCore.trackedMods[curSelected].id);
+				FlxG.save.flush();
+				FlxG.sound.play(Paths.sound('confirmMenu'));
+				changeSelection();
+				changedSomething = !changedSomething;
+			}
+		}
+
+		super.update(elapsed);
+	}
+
+	private function changeSelection(change:Int = 0)
+	{
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+
+		curSelected += change;
+
+		if (curSelected < 0)
+			curSelected = ModCore.trackedMods.length - 1;
+		else if (curSelected >= ModCore.trackedMods.length)
+			curSelected = 0;
+
+		var bullShit:Int = 0;
+
+		if (FlxG.save.data.disabledMods.contains(ModCore.trackedMods[curSelected].id))
+			iconArray[curSelected].alpha = 0.6;
+		else
+			iconArray[curSelected].alpha = 1;
+
+		for (item in daMods.members)
+		{
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			if (FlxG.save.data.disabledMods.contains(ModCore.trackedMods[curSelected].id))
+				item.alpha = 0.6;
+			else
+				item.alpha = 1;
+		}
+	}
+}
+
+class ModIcon extends FlxSprite
+{
+	public var offsetX:Float = 0;
+	public var offsetY:Float = 0;
+	public var sprTracker:FlxSprite;
+
+	public function new(bytes:Bytes)
+	{
+		super();
+
+		loadGraphic(BitmapData.fromBytes(bytes));
+		setGraphicSize(150, 150);
+		updateHitbox();
+		scrollFactor.set();
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-		if(-1 * Math.floor(FlxG.mouse.wheel) != 0)
-		{
-			curSelected -= 1 * Math.floor(FlxG.mouse.wheel);
-			FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-		}
-
-		if (controls.UP_P)
-		{
-			curSelected -= 1;
-			FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-		}
-
-		if (controls.DOWN_P)
-		{
-			curSelected += 1;
-			FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-		}
-
-		if (controls.BACK)
-		{
-			PolymodHandler.loadMods();
-			FlxG.switchState(new MainMenuState());
-		}
-
-		if (curSelected < 0)
-			curSelected = page.length - 1;
-
-		if (curSelected >= page.length)
-			curSelected = 0;
-
-		var bruh = 0;
-
-		for (x in page.members)
-		{
-			x.Alphabet_Text.targetY = bruh - curSelected;
-
-			if(x.Alphabet_Text.targetY == 0)
-			{
-				descriptionText.screenCenter(X);
-
-				@:privateAccess
-				descriptionText.text = 
-				ModList.modMetadatas.get(x.Option_Value).description 
-				+ "\nAuthor: " + ModList.modMetadatas.get(x.Option_Value)._author 
-				+ "\nLeather Engine Version: " + ModList.modMetadatas.get(x.Option_Value).apiVersion 
-				+ "\nMod Version: " + ModList.modMetadatas.get(x.Option_Value).modVersion 
-				+ "\n";
-			}
-
-			bruh++;
-		}
+		if (sprTracker != null)
+			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 25);
 	}
 }
-#end
