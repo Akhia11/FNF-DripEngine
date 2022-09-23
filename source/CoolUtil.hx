d8845baf019e0f9d9854556734a1238e956f8781
kamodrip <100513117+Akhia11@users.noreply.github.com>
2022-09-23 16:24:41 +0300

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -1,32 +1,25 @@
 package;
 
-import lime.utils.Assets;
-
-#if android 
-import sys.io.File;
-import sys.FileSystem;
-#end
+import flixel.FlxG;
+import openfl.utils.Assets;
 
 using StringTools;
 
 class CoolUtil
 {
-	public static var difficultyArray:Array<String> = ['EASY', "NORMAL", "HARD"];
+	public static var difficultyArray:Array<String> = ['Easy', "Normal", "Hard"];
 
-	public static function difficultyString():String
-	{
-		return difficultyArray[PlayState.storyDifficulty];
-	}
+	public static function difficultyString(curDifficulty:Int):String
+		return difficultyArray[curDifficulty];
 
 	public static function coolTextFile(path:String):Array<String>
 	{
-		var daList:Array<String> = []; 
+		var daList:Array<String> = [];
+
                 var daList = File.getContent(SUtil.getPath() + path).trim().split('\n');
-                 
+
 		for (i in 0...daList.length)
-		{
 			daList[i] = daList[i].trim();
-		}
 
 		return daList;
 	}
@@ -34,10 +27,36 @@ class CoolUtil
 	public static function numberArray(max:Int, ?min = 0):Array<Int>
 	{
 		var dumbArray:Array<Int> = [];
+
 		for (i in min...max)
-		{
 			dumbArray.push(i);
-		}
+
 		return dumbArray;
+	}
+
+	public static function camLerpShit(ratio:Float):Float
+		return FlxG.elapsed / (1 / 60) * ratio;
+
+	public static function coolLerp(a:Float, b:Float, ratio:Float):Float
+		return a + camLerpShit(ratio) * (b - a);
+
+	public static function boundTo(value:Float, min:Float, max:Float):Float
+	{
+		var newValue:Float = value;
+
+		if (newValue < min)
+			newValue = min;
+		else if (newValue > max)
+			newValue = max;
+
+		return newValue;
+	}
+
+	public static function truncateFloat(number:Float, precision:Int):Float
+	{
+		var num:Float = number;
+		num = num * Math.pow(10, precision);
+		num = Math.round(num) / Math.pow(10, precision);
+		return num;
 	}
 }
